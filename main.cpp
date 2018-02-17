5ed0bc379ad5759998951213f73152d510f39f8c
Yu Lou <louyu27@uw.edu>
2018-02-17 10:49:26 -0800

    Add comments

--- main.cpp
+++ main.cpp
@@ -13,8 +13,19 @@ struct Obj_Info { // Information of detected object
 const float DISTANCE_DIFFERENCE_THRESHHOLD = 300; // The distance difference tolerance of a single object
 const int AREA_THRESHHOLD = 400; // Minimum number of pixel that can be consider as an object(measued in resized image)
 const float SAFE_DISTANCE = 2000; // Maximum distance that will be reported
+const int RESIZE_WIDTH = 160; // Width of resized image
+const int RESIZE_HEIGHT = 90; // Height of resized image
 
+// Find objects that are too close to the robot
+//
+// Parameters
+//   source: depth map measured by Zed camera
+//   view: depth map converted to RGB
+//
+// Return: List of object informations
 std::vector<Obj_Info> find_objects(cv::Mat source, cv::Mat view);
+
+// Convert Zed camera matrix to OpenCV matrix
 cv::Mat slMat2cvMat(Mat& input);
 void main_zed();
 
@@ -41,10 +52,11 @@ void main_zed()
         exit(-1);
     }
 
-	sl::Mat depth_zed(zed.getResolution(), MAT_TYPE_32F_C1);
+    // Initialize images
+	sl::Mat depth_zed(zed.getResolution(), MAT_TYPE_32F_C1); // Depth map(float)
 	cv::Mat depth_ocv = slMat2cvMat(depth_zed);
 
-	sl::Mat depth_view_zed(zed.getResolution(), MAT_TYPE_8U_C4);
+	sl::Mat depth_view_zed(zed.getResolution(), MAT_TYPE_8U_C4); // Depth map in RGB
 	cv::Mat depth_view_ocv = slMat2cvMat(depth_view_zed);
 
 	cv::Mat depth_view_converted;
@@ -52,13 +64,18 @@ void main_zed()
 	while (true) {
         // Grab an image
         if (zed.grab() == SUCCESS) {
+            // Retrive image
 			zed.retrieveMeasure(depth_zed, MEASURE_DEPTH);
             zed.retrieveImage(depth_view_zed, VIEW_DEPTH);
             std::cout << "Got image" << std::endl;
 
+            // Remove alpha channel
             cv::cvtColor(depth_view_ocv, depth_view_converted, CV_BGRA2BGR);
+            
+            // Find objects
             std::vector<Obj_Info> result = find_objects(depth_ocv, depth_view_converted);
 
+            // Show result
             for (int i = 0; i < result.size(); i++)
             {
                 std::cout << "Position: (" << result[i].boundary.x << ", " << result[i].boundary.y << ")"<< std::endl;
@@ -75,20 +92,21 @@ void main_zed()
 std::vector<Obj_Info> find_objects(cv::Mat source, cv::Mat view)
 {
     // Resize image
-    cv::resize(source, source, cv::Size(160, 90));
-    cv::resize(view, view, cv::Size(160, 90));
+    cv::resize(source, source, cv::Size(RESIZE_WIDTH, RESIZE_HEIGHT));
+    cv::resize(view, view, cv::Size(RESIZE_WIDTH, RESIZE_HEIGHT));
 
     // Deal with special values
-    source.setTo(SAFE_DISTANCE*10, source != source);
-    source.setTo(SAFE_DISTANCE*10, source == std::numeric_limits<float>::infinity());
-    source.setTo(SAFE_DISTANCE*10, source == -std::numeric_limits<float>::infinity());
+    source.setTo(SAFE_DISTANCE*10, source != source); // NaN
+    source.setTo(SAFE_DISTANCE*10, source == std::numeric_limits<float>::infinity()); // Infinity
+    source.setTo(SAFE_DISTANCE*10, source == -std::numeric_limits<float>::infinity()); // Negative infinity
 
-    // Store result of objects
+    // Storage for result
     std::vector<Obj_Info> object_infos;
 
+    // Show original view
     cv::imshow("original", view);
 
-    int width = source.size().width,
+    int width = source.size().width, // Image size
         height = source.size().height;
     cv::Mat filled(height + 2,
                    width + 2,
@@ -110,10 +128,14 @@ std::vector<Obj_Info> find_objects(cv::Mat source, cv::Mat view)
     for (int x = 0; x < width; x++)
         for (int y = 0; y < height; y++)
         {
+            // Iterate through points
             cv::Point point_in_seg(x + 1, y + 1),
                       point_in_source(x, y);
+            
+            // If this area is not marked
             if (filling.at<unsigned char>(point_in_seg) == 0)
             {
+                // Mark adjecent pixels
                 cv::floodFill(source, // image
                               filling, // mask
                               point_in_source, // seed point
@@ -122,35 +144,44 @@ std::vector<Obj_Info> find_objects(cv::Mat source, cv::Mat view)
                               cv::Scalar(DISTANCE_DIFFERENCE_THRESHHOLD), // loDiff
                               cv::Scalar(DISTANCE_DIFFERENCE_THRESHHOLD), // upDiff
                               4 | ((1) << 8) | cv::FLOODFILL_MASK_ONLY | cv::FLOODFILL_FIXED_RANGE); // flags
+                
+                // Get the area filled in this iteration
                 cv::bitwise_xor(filling, filled, difference);
                 invert_difference = cv::Mat::ones(difference.size(), difference.type()) - difference;
+                
+                // Update mark
                 filling.copyTo(filled);
 
-                
+                // Calculate area size
                 int area_size = (int)cv::sum(difference)[0];
 
                 if (area_size > AREA_THRESHHOLD)
                 {
+                    // Measure average distance
                     double distance = cv::mean(source, difference)[0];
                     if (distance < SAFE_DISTANCE)
                     {
+                        // Draw information on image
                         char text[255];
                         cv::Rect boundary = cv::boundingRect(difference);
 
                         sprintf(text, "%.0f", distance);
                         cv::rectangle(result, boundary, boundary_color, 1);
                         cv::putText(result, text, cv::Point(boundary.x, boundary.y+30), cv::FONT_HERSHEY_SCRIPT_SIMPLEX, 0.5, boundary_color);
+                        
+                        // Store information in array
                         object_infos.push_back((Obj_Info){distance, boundary});
                     }
                 }
             }
         }
+    // Show image with information
     std::cout << "show image" << std::endl;
-
 
     cv::imshow("result", result);
     if (cv::waitKey(70) == 0) exit(0);
 
+    // Return
     return object_infos;
 }
 
